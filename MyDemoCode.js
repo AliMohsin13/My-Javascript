5cf9fe5d8bb21ca974944d12f630304ed7f85c68
Ali Mohsin <bsef20m027@pucit.edu.pk>
2023-10-17 00:13:26 +0500

    my first commit

---
+++ MyDemoCode.js
@@ -0,0 +1,12 @@
+// Define variables
+let name = "Ali Mohsin";
+let age = 21;
+
+// Print statements
+console.log("Hello, World!");
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+
+// Perform a simple calculation
+let birthYear = new Date().getFullYear() - age;
+console.log("I was born in the year " + birthYear);
