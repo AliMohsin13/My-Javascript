1d6a3693703bffcb7baa4b53f21610069d01ebdc
Ali Mohsin <bsef20m027@pucit.edu.pk>
2023-10-17 00:20:45 +0500

    my second commit

--- MyDemoCode.js
+++ MyDemoCode.js
@@ -3,6 +3,8 @@ let name = "Ali Mohsin";
 let age = 21;
 
 // Print statements
+console.log("Now I am making changed in my original code.");
+console.log("And now this code will also be commited on Github using git bash command line.");
 console.log("Hello, World!");
 console.log("My name is " + name);
 console.log("I am " + age + " years old");
